05f3517c11d2af43e5c275a4c50de81bc3676401
Diego R. Mirhan <mirhan.diego@gmail.com>
2022-03-07 18:50:32 -0300

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,28 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [ userImput, setUserImput ] = useState("")
-  const [ toDoList, setToDoList ] = useState([])
-
+  const [userImput, setUserImput] = useState('');
+  const [toDoList, setToDoList] = useState([]);
 
   const handleChange = (e) => {
-    setUserImput(e.currentTarget.value)
-  }
-
-  const handleSubmit = (e) => {
-      e.preventDefault()
-      // e.stopPropagation()
-      handleAddTask(userImput)
-      setUserImput("")
-  }
+    setUserImput(e.currentTarget.value);
+  };
 
   const handleAddTask = (e) => {
-    e.preventDefault()
-    if (userImput != "") {
-    setToDoList([...toDoList, userImput])
-    setUserImput("")
+    e.preventDefault();
+    if (userImput !== '') {
+      setToDoList([...toDoList, userImput]);
+      setUserImput('');
     }
-  }
+  };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // e.stopPropagation()
+    handleAddTask(userImput);
+    setUserImput('');
+  };
 
   return (
     <div className="App">
@@ -33,15 +31,28 @@ function App() {
       </header>
       <form onSubmit={handleSubmit} className="Group">
         <div className="form__group field">
-          <input value={userImput} type="text" placeholder="Enter task..." onChange={handleChange} className="form__field" autoComplete="off"/>
+          <input
+            value={userImput}
+            type="text"
+            placeholder="Enter task..."
+            onChange={handleChange}
+            className="form__field"
+            autoComplete="off"
+          />
         </div>
-        <button className="Button" onClick={handleAddTask}>Submit</button>
+        <button className="Button" onClick={handleAddTask} type="button">
+          Submit
+        </button>
       </form>
-      <ul className="todo-list" >
-        {toDoList.map(item => (<div className="Item" key={item}>{item}</div>))}
+      <ul className="todo-list">
+        {toDoList.map((item) => (
+          <div className="Item" key={item}>
+            {item}
+          </div>
+        ))}
       </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
